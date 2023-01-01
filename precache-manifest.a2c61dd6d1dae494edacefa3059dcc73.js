f5f1a66c07f67af3952d9bc6d9a5c77fcaeb7500
abhi1988sharma <60963480+abhi1988sharma@users.noreply.github.com>
2023-01-01 12:22:35 +0000

    Deploying to gh-pages from @ abhi1988sharma/notesKeeperApp@acd26efc0e3d9c2e543a9a70fbb4abc3758d4298 🚀

---
+++ precache-manifest.a2c61dd6d1dae494edacefa3059dcc73.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b878fa3a95cb09f855a7347799a3a34",
+    "url": "./index.html"
+  },
+  {
+    "revision": "9421d0d25647d3079fa8",
+    "url": "./static/js/2.d721fe57.chunk.js"
+  },
+  {
+    "revision": "63879037d5bbd291eb6e",
+    "url": "./static/js/main.1ae73d9e.chunk.js"
+  },
+  {
+    "revision": "5320beb5d47c038ab00d",
+    "url": "./static/js/runtime-main.f219fe86.js"
+  }
+]);
